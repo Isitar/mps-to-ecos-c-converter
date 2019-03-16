91262afd9b9c0a5845247cc48da666d4b5df41de
Pascal Lüscher <luescherpascal@gmail.com>
2019-03-17 00:42:02 +0100

    initial commit with solution

---
+++ MpsToEcosCConverter/MpsToEcosCConverter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MpsToEcosCConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
